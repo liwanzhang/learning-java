f7018be24e08e8c6ee60b64bb3b99d06669fd2f6
dmall <wanli.zhang@dmall.com>
2017-07-02 16:57:45 +0800

    static方法可以继承,不可以覆盖

--- src/main/java/zwl/learning/java/instance/StaticExtends.java
+++ src/main/java/zwl/learning/java/instance/StaticExtends.java
@@ -10,12 +10,19 @@ public class StaticExtends {
     public static void main(String[] args) {
         C c = new D();
         c.f1();
+        c.f2();
+        D d = new D();
+        d.f1();
+        d.f2();
     }
 }
 
 class C {
     public static void f1() {
         System.out.println("C");
+    }
+    public static void f2() {
+        System.out.println("C.f2");
     }
 }
 class D extends C {
